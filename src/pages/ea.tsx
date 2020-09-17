67a4370ff67389dc8b77afae813f20ac3a0c386d
PabloSz <pablosaez1995@gmail.com>
2020-09-17 20:21:10 -0300

    implement react-konva

--- src/pages/ea.tsx
+++ src/pages/ea.tsx
@@ -1,28 +1,55 @@
-import { last, random } from "lodash";
+import { random } from "lodash";
+import { Circle, Layer, Stage } from "react-konva";
 import { createStore } from "react-state-selector";
 
-import { Stack, Text } from "@chakra-ui/core";
+import { Box, Flex, Stack, useColorModeValue } from "@chakra-ui/core";
+
+const width = 700;
+const height = 700;
 
 export const EAStore = createStore({
   data: [
-    { a: 1, b: 2 },
-    { a: 2, b: 4 },
+    {
+      a: random(0, height),
+      b: random(0, width),
+    },
+    {
+      a: random(0, height),
+      b: random(0, width),
+    },
   ],
 });
 
 setInterval(() => {
   EAStore.produce((draft) => {
     draft.data.push({
-      a: (last(draft.data)?.a ?? 0) + 1,
-      b: random(0, 100),
+      a: random(0, height),
+      b: random(0, width),
     });
   });
 }, 1000);
 
 const EAPage = () => {
+  const data = EAStore.useStore().data;
+
+  const bg = useColorModeValue(undefined, "white");
+  const border = useColorModeValue("1px solid black", undefined);
+
   return (
     <Stack>
-      <Text>EA</Text>
+      <Flex justifyContent="center" paddingTop="20px">
+        <Box bg={bg} border={border}>
+          <Stage width={width} height={height}>
+            <Layer>
+              {data.map((v, index) => {
+                return (
+                  <Circle key={index} x={v.a} y={v.b} fill="red" radius={2} />
+                );
+              })}
+            </Layer>
+          </Stage>
+        </Box>
+      </Flex>
     </Stack>
   );
 };
--- src/pages/pso.tsx
+++ src/pages/pso.tsx
@@ -1,28 +1,54 @@
-import { last, random } from "lodash";
+import { random } from "lodash";
+import { Circle, Layer, Stage } from "react-konva";
 import { createStore } from "react-state-selector";
 
-import { Stack, Text } from "@chakra-ui/core";
+import { Box, Flex, Stack, useColorModeValue } from "@chakra-ui/core";
+
+const width = 700;
+const height = 700;
 
 export const PSOStore = createStore({
   data: [
-    { a: 1, b: 2 },
-    { a: 2, b: 4 },
+    {
+      a: random(0, height),
+      b: random(0, width),
+    },
+    {
+      a: random(0, height),
+      b: random(0, width),
+    },
   ],
 });
 
 setInterval(() => {
   PSOStore.produce((draft) => {
     draft.data.push({
-      a: (last(draft.data)?.a ?? 0) + 1,
-      b: random(0, 100),
+      a: random(0, height),
+      b: random(0, width),
     });
   });
-}, 3000);
+}, 1000);
 
 const PSOPage = () => {
+  const data = PSOStore.useStore().data;
+
+  const bg = useColorModeValue(undefined, "white");
+  const border = useColorModeValue("1px solid black", undefined);
   return (
     <Stack>
-      <Text>PSO</Text>
+      <Flex justifyContent="center" paddingTop="20px">
+        <Box bg={bg} border={border}>
+          <Stage width={width} height={height}>
+            <Layer>
+              {data.map((v, index) => {
+                return (
+                  <Circle key={index} x={v.a} y={v.b} fill="green" radius={2} />
+                );
+              })}
+            </Layer>
+          </Stage>
+        </Box>
+      </Flex>
     </Stack>
   );
 };
