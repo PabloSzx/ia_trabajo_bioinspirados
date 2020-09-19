e2095308decbff051c3da82f444a2d1ea3b8c736
PabloSz <pablosaez1995@gmail.com>
2020-09-19 16:50:21 -0300

    optimize pso implementation

--- src/pages/pso.tsx
+++ src/pages/pso.tsx
@@ -1,7 +1,7 @@
 import { minBy, range } from "lodash";
-import { Fragment } from "react";
+import { Fragment, memo } from "react";
 import { Circle, Layer, Line, Stage } from "react-konva";
-import { createStore } from "react-state-selector";
+import { createSelector, createStore } from "react-state-selector";
 
 import {
   Box,
@@ -52,6 +52,21 @@ export const PSOStore = createStore(
   },
   {
     hooks: {
+      useMetadata: createSelector(
+        (state) => state.inertia,
+        (state) => state.maxVelocity,
+        (state) => state.n,
+        (inertia, maxVelocity, n) => {
+          return {
+            inertia,
+            maxVelocity,
+            n,
+          };
+        }
+      ),
+      useData(store) {
+        return store.data;
+      },
       useBestHistory(store) {
         return store.bestHistory;
       },
@@ -116,42 +131,45 @@ export const PSOStore = createStore(
         let fitnessImproved = false;
 
         for (const value of draft.data) {
-          value.vx1 =
-            inertia * value.vx1 +
-            getRandomPercent() * (value.bestPersonalx1 - value.x1) +
-            getRandomPercent() * (draft.bestX1 - value.x1);
-          value.vx2 =
-            inertia * value.vx2 +
-            getRandomPercent() * (value.bestPersonalx2 - value.x2) +
-            getRandomPercent() * (draft.bestX2 - value.x2);
+          let x1 = value.x1;
+          let x2 = value.x2;
 
-          const mod = Math.sqrt(value.vx1 * value.vx1 + value.vx2 * value.vx2);
+          let vx1 =
+            inertia * value.vx1 +
+            getRandomPercent() * (value.bestPersonalx1 - x1) +
+            getRandomPercent() * (draft.bestX1 - x1);
+          let vx2 =
+            inertia * value.vx2 +
+            getRandomPercent() * (value.bestPersonalx2 - x2) +
+            getRandomPercent() * (draft.bestX2 - x2);
+
+          const mod = Math.sqrt(vx1 * vx1 + vx2 * vx2);
 
           if (mod > maxVelocity) {
-            value.vx1 /= mod * maxVelocity;
-            value.vx2 /= mod * maxVelocity;
+            vx1 /= mod * maxVelocity;
+            vx2 /= mod * maxVelocity;
           }
 
-          value.x1 += value.vx1;
-          value.x2 += value.vx2;
+          x1 += vx1;
+          x2 += vx2;
 
-          if (value.x1 > 5.12) {
-            value.x1 = 5.12;
-          } else if (value.x1 < -5.12) {
-            value.x1 = -5.12;
+          if (x1 > 5.12) {
+            x1 = 5.12;
+          } else if (x1 < -5.12) {
+            x1 = -5.12;
           }
-          if (value.x2 > 5.12) {
-            value.x2 = 5.12;
-          } else if (value.x2 < -5.12) {
-            value.x2 = -5.12;
+          if (x2 > 5.12) {
+            x2 = 5.12;
+          } else if (x2 < -5.12) {
+            x2 = -5.12;
           }
 
-          const fitness = calcRastrigin(value.x1, value.x2);
+          const fitness = calcRastrigin(x1, x2);
 
           if (fitness < value.bestPersonalY) {
             value.bestPersonalY = fitness;
-            value.bestPersonalx1 = value.x1;
-            value.bestPersonalx2 = value.x2;
+            value.bestPersonalx1 = x1;
+            value.bestPersonalx2 = x2;
           }
 
           if (fitness < draft.bestY) {
@@ -161,6 +179,11 @@ export const PSOStore = createStore(
 
             fitnessImproved = true;
           }
+
+          value.x1 = x1;
+          value.x2 = x2;
+          value.vx1 = vx1;
+          value.vx2 = vx2;
         }
 
         if (fitnessImproved) {
@@ -183,8 +206,36 @@ if (typeof window !== "undefined")
     PSOStore.actions.tick();
   }, 20);
 
+const Canvas = memo(() => {
+  const data = PSOStore.hooks.useData();
+  return (
+    <Stage width={width} height={height}>
+      <Layer>
+        {data
+          .map((v) => ({
+            ...v,
+            x1: v.x1 * 67 + 350,
+            x2: v.x2 * 67 + 350,
+          }))
+          .map((v, index) => {
+            return (
+              <Fragment key={index}>
+                <Circle key={index} x={v.x1} y={v.x2} fill="green" radius={4} />
+                <Line
+                  key={index}
+                  points={[v.x1, v.x2, v.x1 - 50 * v.vx1, v.x2 - 50 * v.vx2]}
+                  stroke="green"
+                />
+              </Fragment>
+            );
+          })}
+      </Layer>
+    </Stage>
+  );
+});
+
 const PSOPage = () => {
-  const { data, inertia, maxVelocity, n } = PSOStore.useStore();
+  const { inertia, maxVelocity, n } = PSOStore.hooks.useMetadata();
 
   const bg = useColorModeValue(undefined, "white");
   const border = useColorModeValue("1px solid black", undefined);
@@ -257,39 +308,7 @@ const PSOPage = () => {
 
       <Flex justifyContent="center" paddingTop="20px">
         <Box bg={bg} border={border}>
-          <Stage width={width} height={height}>
-            <Layer>
-              {data
-                .map((v) => ({
-                  ...v,
-                  x1: v.x1 * 67 + 350,
-                  x2: v.x2 * 67 + 350,
-                }))
-                .map((v, index) => {
-                  return (
-                    <Fragment key={index}>
-                      <Circle
-                        key={index}
-                        x={v.x1}
-                        y={v.x2}
-                        fill="green"
-                        radius={4}
-                      />
-                      <Line
-                        key={index}
-                        points={[
-                          v.x1,
-                          v.x2,
-                          v.x1 - 50 * v.vx1,
-                          v.x2 - 50 * v.vx2,
-                        ]}
-                        stroke="green"
-                      />
-                    </Fragment>
-                  );
-                })}
-            </Layer>
-          </Stage>
+          <Canvas />
         </Box>
       </Flex>
     </Stack>
